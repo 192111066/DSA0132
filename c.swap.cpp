ae9759e512c8e7f2c63fe93807ff3a4d31873ec6
192111066 <112462867+192111066@users.noreply.github.com>
2022-10-19 14:37:43 +0530

    Add files via upload

---
+++ c.swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x=20,y=30;
+	cout<<"before swapping""<<end1;
+	cout<<"a="<<x<<",y="<<0y<<end1;
+	x=x+y;
+	y=x-y;
+	y=x-y;
+	cout<<"/n after swapping"end1;
+	cout<<"x="<<x<<",y="<<end;
+	return 0;
+}
